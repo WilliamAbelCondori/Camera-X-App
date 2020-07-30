7b6ad5735bb6bc86da8b002b68270823a5aeb569
WilliamAbelCondori <1williamcondori1@gmail.com>
2020-07-29 21:44:15 -0400

    Implement ImageCapture

--- app/src/main/java/com/william/camerax_app/MainActivity.kt
+++ app/src/main/java/com/william/camerax_app/MainActivity.kt
@@ -22,7 +22,6 @@ import java.util.*
 import java.util.concurrent.ExecutorService
 
 
-
 class MainActivity : AppCompatActivity() {
     private var preview: Preview? = null
     private var imageCapture: ImageCapture? = null
@@ -65,6 +64,8 @@ class MainActivity : AppCompatActivity() {
             // Preview
             preview = Preview.Builder().build()
 
+            imageCapture = ImageCapture.Builder().build()
+
             // Select back camera
             val cameraSelector =
                 CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
@@ -75,9 +76,10 @@ class MainActivity : AppCompatActivity() {
 
                 // Bind use cases to camera
                 camera = cameraProvider.bindToLifecycle(
-                    this, cameraSelector, preview
+                    this, cameraSelector, preview, imageCapture
                 )
-                preview?.setSurfaceProvider( viewFinder.createSurfaceProvider())
+                preview?.setSurfaceProvider(viewFinder.createSurfaceProvider())
+
             } catch (exc: Exception) {
                 Log.e(TAG, "Use case binding failed", exc)
             }
@@ -86,7 +88,37 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun takePhoto() {
-        TODO("Not yet implemented")
+        // Get a stable reference of the modifiable image capture use case
+        val imageCapture = imageCapture ?: return
+
+        // Create timestamped output file to hold the image
+        val photoFile = File(
+            outputDirectory,
+            SimpleDateFormat(
+                FILENAME_FORMAT, Locale.US
+            ).format(System.currentTimeMillis()) + ".jpg"
+        )
+
+        // Create output options object which contains file + metadata
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+        // Setup image capture listener which is triggered after photo has
+        // been taken
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exc: ImageCaptureException) {
+                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+                }
+
+                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                    val savedUri = Uri.fromFile(photoFile)
+                    val msg = "Photo capture succeeded: $savedUri"
+                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                    Log.d(TAG, msg)
+                }
+            })
     }
 
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
